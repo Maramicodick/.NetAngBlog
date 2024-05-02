411fffa7b37c8ab9e9e409f5d0d03d1be07c841b
Aleksandr <sashamacinovich@gmail.com>
2024-05-02 17:49:26 +0300

    Implementer repository pattern

--- API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodePulse.API.Controllers
@@ -7,5 +11,32 @@ namespace CodePulse.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto requestDto)
+        {
+            var category = new Category
+            {
+                Name = requestDto.Name,
+                UrlHandle = requestDto.UrlHandle
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
     }
 }
---
+++ API/CodePulse.API/CodePulse.API/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.Interface;
+
+namespace CodePulse.API.Repositories.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public readonly ApplicationDbContext dbContext;
+
+        public CategoryRepository(ApplicationDbContext dbContext) 
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            return category;
+        }
+    }
+}
