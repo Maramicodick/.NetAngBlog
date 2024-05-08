d8ea6c0049a640910bcef9cda06f2b4488070f6a
Aleksandr <sashamacinovich@gmail.com>
2024-05-08 18:04:13 +0300

    Created add-category component

--- UI/codepulse/src/app/app.component.ts
+++ UI/codepulse/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, RouterModule],
+  imports: [RouterOutlet, NavbarComponent, RouterModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
