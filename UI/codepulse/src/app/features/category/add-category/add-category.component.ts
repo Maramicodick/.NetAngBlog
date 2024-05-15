5def38755bc6436899421da60cf5abca4b4330a8
Aleksandr <sashamacinovich@gmail.com>
2024-05-15 15:16:32 +0300

    Added observable unsubscription

--- UI/codepulse/src/app/features/category/add-category/add-category.component.ts
+++ UI/codepulse/src/app/features/category/add-category/add-category.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
@@ -10,9 +11,10 @@ import { CategoryService } from '../services/category.service';
   templateUrl: './add-category.component.html',
   styleUrl: './add-category.component.css'
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
 
   model: AddCategoryRequest;
+  private addCategorySubscribtion?: Subscription;
 
   constructor(private categoryService: CategoryService) {
     this.model = {
@@ -22,12 +24,17 @@ export class AddCategoryComponent {
   }
 
   onFormSubmit() {
-    this.categoryService.addCategory(this.model)
+    this.addCategorySubscribtion = this.categoryService.addCategory(this.model)
     .subscribe({
       next: (response) => {
         console.log("This was Succesfull")
       }
     })
-  }  
+  }
+  
+  ngOnDestroy(): void {
+    this.addCategorySubscribtion?.unsubscribe();
+  }
+
 }
 
